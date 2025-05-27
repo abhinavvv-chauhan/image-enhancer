3ae65af980b79bb10e78458ecc6a1b8d6785f6ff
abhinav <abhinavchauhan593@gmail.com>
2025-05-28 00:45:02 +0530

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ImagePreview from './ImagePreview'
+import ImageUpload from './ImageUpload'
+import { enhancedImageAPI } from "../utils/enhanceImageApi";
+import { useState } from 'react'
+
+
+const Home = () => {
+    const [uploadImage, setUploadImage] = useState(null);
+    const [enhancedImage, setEnhancedImage] = useState(null);
+    const [loading, setloading] = useState(false);
+
+    const UploadImageHandler = async (file) => {
+        setUploadImage(URL.createObjectURL(file));
+        setloading(true);
+        try {
+            const enhancedURL = await enhancedImageAPI(file);
+            setEnhancedImage(enhancedURL);
+            setloading(false);
+        } catch (error) {
+            console.log(error);
+            alert("Error while enhancing the image. Please try again later.");
+        }
+    };
+
+    return (
+        <>
+            <ImageUpload UploadImageHandler={UploadImageHandler} />
+            <ImagePreview
+                loading={loading}
+                uploaded={uploadImage}
+                enhanced={enhancedImage?.image}
+            />
+        </>
+    );
+};
+
+export default Home;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center h-full'>
+      <div className='border-4 border-t-transparent rounded-full border-red-300 animate-spin w-10 h-10 scale-200'></div>
+    </div>
+  ) 
+    
+  
+}
+
+export default Loading
