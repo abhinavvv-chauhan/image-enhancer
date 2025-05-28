53822f9beeb22f0209073da9a99a2f83f7480041
abhinav <abhinavchauhan593@gmail.com>
2025-05-28 11:10:37 +0530

    secure: added .env to .gitignore and cleaned project

--- src/utils/enhanceImageApi.js
+++ src/utils/enhanceImageApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "wx7okucfcve9eos0l";
+const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://techhk.aoscdn.com/";
 const MAXIMUM_RETRIES = 20;
 
